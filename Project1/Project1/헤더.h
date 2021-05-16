36c82d0ae59aa512c17437b5d3923e3d19492c4e
swpang10 <82203119+swpang10@users.noreply.github.com>
2021-05-16 17:12:29 +0900

    Add files via upload

---
+++ Project1/Project1/헤더.h
@@ -0,0 +1,472 @@
+#pragma once
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+
+
+class Node
+{
+private:
+
+	Node* next;
+	bool check;
+	char* binary;
+	unsigned short one_num;
+
+public:
+	Node();
+	~Node();
+	Node(unsigned short);
+	Node* getNext();
+	bool getCheck();
+	void setCheck(bool);
+	void setNext(Node* nextIn);
+	char* getBinary();
+	void setBinary(char*);
+	unsigned short getOneNum();
+
+};
+
+class Link
+{
+private:
+	Node* pHead;
+public:
+	Link();
+	~Link();
+	Node* getHead();
+	void setHead(Node* headIn);
+	void addNode(unsigned short bit_legth, char* input_binary);
+	void print();
+	void Find(Link*, unsigned short);
+	void groupCompare(Node*, Node*, unsigned short);
+	void compareBinary(Node* compare1, Node* compare2, unsigned short bit_length);
+};
+
+
+
+//Node's method functions
+
+Node::Node()
+{
+	check = false;
+	binary = NULL;
+	next = nullptr;
+	one_num = 0;
+}
+Node::~Node()
+{
+
+}
+Node::Node(unsigned short length)
+{
+	check = false;
+	next = nullptr;
+	one_num = 0;
+	binary = new char[length];
+}
+bool Node::getCheck()
+{
+	return check;
+}
+void Node::setCheck(bool checkIn)
+{
+	check = checkIn;
+}
+Node* Node::getNext()
+{
+	return next;
+}
+void Node::setNext(Node* nextIn)
+{
+	next = nextIn;
+}
+char* Node::getBinary()
+{
+	return binary;
+}
+void Node::setBinary(char* binaryIn)
+{
+
+	int i = 0;
+	while (binaryIn[i] != '\0')
+	{
+		if ((binary[i] = binaryIn[i]) == '1')
+			++one_num;
+		++i;
+	}
+	binary[i] = '\0';
+}
+unsigned short Node::getOneNum()
+{
+	return one_num;
+}
+
+
+//Link's method functions
+
+Link::Link()
+{
+	pHead = nullptr;
+}
+Link::~Link()
+{
+	Node* tempNode = pHead;
+	while (pHead)
+	{
+		pHead = pHead->getNext();
+	}
+}
+Node* Link::getHead()
+{
+	return pHead;
+}
+void Link::setHead(Node* headIn)
+{
+	pHead = headIn;
+}
+void Link::addNode(unsigned short bit_length, char* input_binary)
+{
+	Node* newNode = new Node(bit_length);
+	newNode->setBinary(input_binary);
+
+	if (pHead == nullptr)
+	{
+		pHead = newNode;
+	}
+	else
+	{
+		Node* curNode = pHead;
+
+		while (curNode)
+		{
+			if (strcmp(curNode->getBinary(), newNode->getBinary()) == 0)
+				return;
+
+			if (curNode == pHead)
+			{
+				if (curNode->getOneNum() > newNode->getOneNum())
+				{
+					newNode->setNext(curNode);
+					pHead = newNode;
+					return;
+				}
+			}
+			if (curNode->getNext() == nullptr)
+			{
+				curNode->setNext(newNode);
+				newNode->setNext(nullptr);
+				return;
+			}
+			if (newNode->getOneNum() < curNode->getNext()->getOneNum())
+			{
+				newNode->setNext(curNode->getNext());
+				curNode->setNext(newNode);
+				return;
+			}
+			curNode = curNode->getNext();
+		}
+
+	}
+
+
+}
+void Link::print()
+{
+	Node* tempNode = pHead;
+
+	while (tempNode)
+	{
+		cout << "-->  " << tempNode->getBinary() << "\n";
+		tempNode = tempNode->getNext();
+	}
+}
+void Link::Find(Link* PI, unsigned short bit_length)
+{
+	Node* preMark = pHead;
+	Node* postMark = pHead;
+
+	Link compared;
+
+	while (postMark)
+	{
+		if ((postMark->getOneNum() - preMark->getOneNum()) == 1)
+		{
+			compared.groupCompare(preMark, postMark, bit_length);
+		}
+
+		if ((postMark->getOneNum() - preMark->getOneNum()) != 0)
+		{
+			while (preMark != postMark)
+			{
+				if (preMark->getCheck() == false)
+				{
+					PI->addNode(bit_length, preMark->getBinary());
+				}
+				preMark = preMark->getNext();
+			}
+		}
+
+		postMark = postMark->getNext();
+	}
+
+	while (preMark != NULL)
+	{
+		if (preMark->getCheck() == false)
+		{
+			PI->addNode(bit_length, preMark->getBinary());
+		}
+		preMark = preMark->getNext();
+
+	}
+
+	if (compared.getHead() == NULL)
+	{
+		return;
+	}
+
+	compared.Find(PI, bit_length);
+	return;
+}
+void Link::groupCompare(Node* preNode, Node* postNode, unsigned short bit_length)
+{
+	Node* tempPre = preNode;
+	Node* tempPost = postNode;
+
+	while (preNode->getOneNum() == tempPre->getOneNum())
+	{
+		while (tempPost != NULL && postNode->getOneNum() == tempPost->getOneNum())
+		{
+			compareBinary(tempPre, tempPost, bit_length);
+			tempPost = tempPost->getNext();
+		}
+		tempPre = tempPre->getNext();
+		tempPost = postNode;
+	}
+
+}
+void Link::compareBinary(Node* compare1, Node* compare2, unsigned short bit_length)
+{
+	int dif = 0;
+	char* result = new char[bit_length];
+	for (int i = 0; i < bit_length; ++i)
+	{
+		if (compare1->getBinary()[i] != compare2->getBinary()[i])
+		{
+			if (compare1->getBinary()[i] == '-' || compare2->getBinary()[i] == '-')
+			{
+				delete[] result;
+				return;
+			}
+			result[i] = '-';
+			dif++;
+		}
+		else
+		{
+			result[i] = compare1->getBinary()[i];
+		}
+	}
+	if (dif == 1)
+	{
+		addNode(bit_length, result);
+		compare1->setCheck(true);
+		compare2->setCheck(true);
+	}
+	delete[] result;
+	return;
+}
+
+
+
+/*
+class EPI {
+	int temp = 10000;
+	unsigned short bit_length = 4;
+public:
+	char** aaa;
+	void TranNum(char *** Epim) {
+		int tran = 0;
+		int tall = 0;
+		for (int i = 0; Epim[i] == NULL; i++) {
+			for (int t = 0; Epim[t][i] == NULL; i++) {
+				tall++;
+			}
+		}
+
+		for (int t = 0; t == 2; t++) {
+
+
+			for (int i = 0; Epim[t][i] == NULL; i++) {
+				tall++;
+			}
+
+			for (int i = 0; i < bit_length; i++) {
+
+
+
+				for (int k = 0; k < tall; k++) {
+					if (Epim[t][k][i] == '0') {
+						tran += 2;
+						break;
+					}
+				}
+			}
+
+			tran = tran + tall * 2 + 2;
+
+			if (temp > tran) {
+				aaa = Epim[t];
+				temp = tran;
+			}
+			tran = 0;
+			tall = 0;
+
+		}
+
+
+		for (int i = 0; aaa[i] = NULL; i++) {
+			cout << aaa[i] << "\t";
+		}
+		cout << "Transister Number : " << temp << endl;
+
+	};
+	
+
+};
+
+*/
+
+void TranNum(char*** Epim, unsigned short bit_length) {
+	char** aaa = NULL;
+
+	aaa = new char* [4];
+	for (int i = 0; i < 4; i++) {
+		aaa[i] = new char[4];
+	}
+
+
+	int temp = 1111111;
+	int tran = 0;
+	int tall = 0;
+
+	for (int i = 0; Epim[i] == NULL; i++) {
+		for (int t = 0; Epim[t][i] == NULL; i++) {
+			tall++;
+		}
+	}
+
+	for (int t = 0; t < 3; t++) {
+
+
+		for (int i = 0; Epim[t][i] == NULL; i++) {
+			tall++;
+		}
+
+		for (int i = 0; i < bit_length; i++) {
+
+
+
+			for (int k = 0; k < tall; k++) {
+				if (Epim[t][k][i] == '0') {
+					tran += 2;
+					break;
+				}
+			}
+		}
+
+		tran = tran + tall * 2 + 2;
+		cout << tran << endl;
+
+		if (temp > tran) {
+			aaa = Epim[t];
+			temp = tran;
+			//cout << temp << endl;
+		}
+
+		tran = 0;
+		tall = 0;
+
+	}
+
+
+	for (int i = 0; aaa[i] == NULL; i++) {
+		for (int k = 0; aaa[i][k] == NULL; k++) {
+			cout << aaa[i][k];
+		}
+		cout << "+";
+	}
+
+
+	cout << "Transister Number : " << temp << endl;
+
+};
+/*
+int main(void)
+{
+
+	char*** Epim = NULL;
+
+
+	Epim = new char** [4];
+	for (int x = 0; x < 4; x++)
+	{
+		Epim[x] = new char* [4];
+		for (int y = 0; y < 4; y++)
+		{
+			Epim[x][y] = new char[4];
+		}
+	}
+	/*
+
+	char e[4][4][4] = { {'1','1','1','1'}, {'1','1','1','0'},  {'1','1','0','1',} ,{'1','0','1','1'} };
+
+	Epim[0][0][0] = '1';
+	Epim[0][0][1] = '1';
+	Epim[0][0][2] = '1';
+	Epim[0][0][3] = '1';
+
+	Epim[0][1][0] = '_';
+	Epim[0][1][1] = '1';
+	Epim[0][1][2] = '1';
+	Epim[0][1][3] = '_';
+
+	Epim[0][2][0] = '0';
+	Epim[0][2][1] = '1';
+	Epim[0][2][2] = '1';
+	Epim[0][2][3] = '1';
+	//--------------------------------------
+	Epim[1][0][0] = '-';
+	Epim[1][0][1] = '-';
+	Epim[1][0][2] = '-';
+	Epim[1][0][3] = '1';
+
+	Epim[1][1][0] = '0';
+	Epim[1][1][1] = '1';
+	Epim[1][1][2] = '1';
+	Epim[1][1][3] = '-';
+
+	Epim[1][2][0] = '1';
+	Epim[1][2][1] = '1';
+	Epim[1][2][2] = '1';
+	Epim[1][2][3] = '0';
+	//-----------------------------------
+	Epim[2][0][0] = '1';
+	Epim[2][0][1] = '1';
+	Epim[2][0][2] = '1';
+	Epim[2][0][3] = '1';
+
+	Epim[2][1][0] = '0';
+	Epim[2][1][1] = '1';
+	Epim[2][1][2] = '1';
+	Epim[2][1][3] = '1';
+
+	Epim[2][2][0] = '0';
+	Epim[2][2][1] = '-';
+	Epim[2][2][2] = '1';
+	Epim[2][2][3] = '-';
+	
+	TranNum(Epim, 4);
+}*/
